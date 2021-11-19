6022bff8e497d6c993b05af99fe56df8eb790a19
lucasftassi <lucasftassi@outlook.com>
2021-11-19 18:18:00 -0300

    Funcao para bater no esp32

---
+++ webapi.py
@@ -0,0 +1,23 @@
+from pip._vendor import requests
+
+url = "http://192.168.15.17"
+
+def capturarImagem():
+    api_url = url + "/capturar-foto"
+    response = requests.get(api_url)
+
+    print(response.status_code)
+
+    return response
+
+def obterImagem():
+    api_url = url + "http://192.168.15.17/foto-salva"
+    response = requests.get(api_url)
+    
+    if response.status_code == 200:
+        with open("fotos-capturadas/image.jpg", 'wb') as f:
+            f.write(response.content)
+
+    return response
+
+obterImagem()
